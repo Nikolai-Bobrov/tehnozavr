620da0a9bd87a31ad8677be440645cdef6709ee2
Николай Бобров <m4skarpone@yandex.ru>
2023-07-11 16:56:45 +1000

    Подлючение Vue-Router(коректировка)

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,6 @@
 <script>
 import MainPage from "@/pages/MainPage.vue";
 import ProductPage from "@/pages/ProductPage.vue";
-import NotFoundPage from "@/pages/NotFoundPage.vue";
-import eventBus from "@/eventBus";
 
 const routes = {
   main: 'MainPage',
@@ -16,27 +14,7 @@ const routes = {
 }
 
 export default {
-  //   data(){
-  //     return {
-  //       currentPage: 'main',
-  //       currentPageParams:{}
-  //     }
-  //   },
-  // methods:{
-  //     gotoPage(pageName, pageParams){
-  //       this.currentPage = pageName;
-  //       this.currentPageParams = pageParams || {};
-  //     }
-  // },
-  // computed:{
-  //     currentPageComponent(){
-  //       return routes[this.currentPage] || 'NotFoundPage';
-  //     }
-  // },
-  // components: {MainPage, ProductPage, NotFoundPage},
-  // created() {
-  //     eventBus.$on('gotoPage',(pageName, pageParams)=> this.gotoPage(pageName, pageParams))
-  // }
+
 }
 </script>
 
