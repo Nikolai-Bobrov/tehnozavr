3c0dc25a279783c03a60a21c0cfe0a37ae9d2cd0
Николай Бобров <m4skarpone@yandex.ru>
2023-07-18 17:06:31 +1000

    The order has been made

--- src/components/HeaderTag.vue
+++ src/components/HeaderTag.vue
@@ -4,7 +4,7 @@
       <span class="header__info">Каталог</span>
 
       <router-link class="header__logo" :to="{name: 'main'}" href="#">
-        <img src="./img/svg/logo-tech.svg" alt="Логотип интернет магазина Технозавррр" width="190" height="33">
+        <img src="/tehnozavr-app/img/svg/logo-tech.svg" alt="Логотип интернет магазина Технозавррр" width="190" height="33">
       </router-link>
 
       <a class="header__tel" href="tel:8 800 600 90 09">
